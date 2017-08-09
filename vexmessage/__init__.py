d86742a4de5025a826447ae94a7ae70366c374f5
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-08-09 04:56:17 +0300

    wip

--- vexmessage/__init__.py
+++ vexmessage/__init__.py
@@ -2,36 +2,15 @@ import pickle as _pickle
 import textwrap as _textwrap
 
 
-VERSION = '0.2.0'
-
-
-def decode(frame):
-    target = frame[0].decode('ascii')
-    deserial = _pickle.loads(frame[1])
-    source = deserial[0]
-    type = deserial[1]
-    version = deserial[2]
-    content = deserial[3]
-
-    return Message(target, source, type, version, **content)
-
-
-def encode(target, source, type, version=VERSION, **message):
-    target = target.encode('ascii')
-    serialization = _pickle.dumps((source, type, version, message))
-    return (target, serialization)
-
-
-def create_vex_message(target, source, type, version=VERSION, **msg):
-    return encode(target, source, type, version, **msg)
-
-
-def decode_vex_message(frame):
-    return decode(frame)
-
+VERSION = '0.3.0'
 
 class Message:
-    def __init__(self, target, source, type, version=VERSION, **content):
+    def __init__(self,
+                 target: str,
+                 source, type,
+                 version: str=VERSION,
+                 **content):
+
         self.target = target
         self.source = source
         self.type = type
@@ -50,6 +29,62 @@ class Message:
                      self.contents)
 
         return _textwrap.fill(s)
+
+def decode(frame) -> Message:
+    target = frame[0].decode('ascii')
+    deserial = _pickle.loads(frame[1])
+    source = deserial[0]
+    type = deserial[1]
+    version = deserial[2]
+    content = deserial[3]
+
+    return Message(target, source, type, version, **content)
+
+
+def encode(target: str,
+           source: str,
+           type: str,
+           version: str=VERSION,
+           **message) -> tuple:
+
+    target = target.encode('ascii')
+    serialization = _pickle.dumps((source, type, version, message))
+    return (target, serialization)
+
+
+def create_vex_message(target: str,
+                       source: str,
+                       type: str,
+                       version: str=VERSION,
+                       **msg) -> Message:
+
+    return encode(target, source, type, version, **msg)
+
+
+def decode_vex_message(frame):
+    return decode(frame)
+
+def create_request(source: str, type: str, version: str=VERSION, **request):
+    source = source.encode('ascii')
+    serialization = _pickles.dumps(('', type, version, request))
+    return (source, serialization)
+
+
+class ReqMessage:
+    def __init__(self,
+                 command: str,
+                 source: str,
+                 version: str=VERSION,
+                 *args,
+                 **kwargs):
+
+        self.source = source
+        self.command = command
+        self.version = version
+        self.args = args
+        self.kwargs = kwargs
+
+
 
 
 class VexTypes:
